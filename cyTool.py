0206289a342c5cbfd8fc4232f3cea4784ddb7a99
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2024-07-05 17:57:01 +0530

    Update cyTool.py

--- cyTool.py
+++ cyTool.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 import lib.playfair as playfair
 import lib.vignere as vignere
 import lib.rot as rot
@@ -14,40 +15,48 @@ def createParse():
     parser.add_argument('-rf', '--railfence', help='Railfence cipher key. Syntax: [-rf <key>], [--railfence <key>]')
     return parser
 
-p = createParse()
-args = p.parse_args()
+def main():
+    p = createParse()
+    args = p.parse_args()
 
-if args.encrypt:
-    if args.rotation:
-        res = rot.encrypt(args.encrypt, int(args.rotation))
-        print(f'Encrypting "{args.encrypt}" using Rotation cipher with key = {args.rotation}')
-        print(f'Result = {res}')
-    elif args.vigenere:
-        res = vignere.encrypt(args.encrypt, args.vigenere)
-        print(f'Encrypting "{args.encrypt}" using Vigenere cipher with keyword = "{args.vigenere}"')
-        print(f'Result = {res}')
-    elif args.playfair:
-        res = playfair.encrypt(args.encrypt, args.playfair)
-        print(f'Encrypting "{args.encrypt}" using Playfair cipher with keyword = "{args.playfair}"')
-        print(f'Result = {res}')
-    elif args.railfence:
-        res = railfence.encrypt(args.encrypt, int(args.railfence))
-        print(f'Encrypting "{args.encrypt}" using Railfence cipher with depth = {args.railfence}')
-        print(f'Result = {res}')
-elif args.decrypt:
-    if args.rotation:
-        res = rot.decrypt(args.decrypt, int(args.rotation))
-        print(f'Decrypting "{args.decrypt}" using Rotation cipher with key = {args.rotation}')
-        print(f'Result = {res}')
-    elif args.vigenere:
-        res = vignere.decrypt(args.decrypt, args.vigenere)
-        print(f'Decrypting "{args.decrypt}" using Vigenere cipher with keyword = "{args.vigenere}"')
-        print(f'Result = {res}')
-    elif args.playfair:
-        res = playfair.decrypt(args.decrypt, args.playfair)
-        print(f'Decrypting "{args.decrypt}" using Playfair cipher with keyword = "{args.playfair}"')
-        print(f'Result = {res}')
-    elif args.railfence:
-        res = railfence.decrypt(args.decrypt, int(args.railfence))
-        print(f'Decrypting "{args.decrypt}" using Railfence cipher with depth = {args.railfence}')
-        print(f'Result = {res}')
+    if len(sys.argv) > 1:
+        if args.encrypt:
+            if args.rotation:
+                res = rot.encrypt(args.encrypt, int(args.rotation))
+                print(f'Encrypting "{args.encrypt}" using Rotation cipher with key = {args.rotation}')
+                print(f'Result = {res}')
+            elif args.vigenere:
+                res = vignere.encrypt(args.encrypt, args.vigenere)
+                print(f'Encrypting "{args.encrypt}" using Vigenere cipher with keyword = "{args.vigenere}"')
+                print(f'Result = {res}')
+            elif args.playfair:
+                res = playfair.encrypt(args.encrypt, args.playfair)
+                print(f'Encrypting "{args.encrypt}" using Playfair cipher with keyword = "{args.playfair}"')
+                print(f'Result = {res}')
+            elif args.railfence:
+                res = railfence.encrypt(args.encrypt, int(args.railfence))
+                print(f'Encrypting "{args.encrypt}" using Railfence cipher with depth = {args.railfence}')
+                print(f'Result = {res}')
+        elif args.decrypt:
+            if args.rotation:
+                res = rot.decrypt(args.decrypt, int(args.rotation))
+                print(f'Decrypting "{args.decrypt}" using Rotation cipher with key = {args.rotation}')
+                print(f'Result = {res}')
+            elif args.vigenere:
+                res = vignere.decrypt(args.decrypt, args.vigenere)
+                print(f'Decrypting "{args.decrypt}" using Vigenere cipher with keyword = "{args.vigenere}"')
+                print(f'Result = {res}')
+            elif args.playfair:
+                res = playfair.decrypt(args.decrypt, args.playfair)
+                print(f'Decrypting "{args.decrypt}" using Playfair cipher with keyword = "{args.playfair}"')
+                print(f'Result = {res}')
+            elif args.railfence:
+                res = railfence.decrypt(args.decrypt, int(args.railfence))
+                print(f'Decrypting "{args.decrypt}" using Railfence cipher with depth = {args.railfence}')
+                print(f'Result = {res}')
+    else:
+        print("Welcome to CyTool!")
+        print("Run 'py cyTool.py -h' for help")
+
+if __name__ == "__main__":
+    main()
